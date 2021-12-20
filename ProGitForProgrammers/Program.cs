a67e3d0e805e835692aa1a3c892d5ae578d46121
Reede Stockton <sfdc@willriley.com>
2021-12-19 20:38:14 -0800

    New commit

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -5,7 +5,7 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in VS Code");
         }
     }
 }
